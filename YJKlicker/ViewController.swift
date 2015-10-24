84309dd273eb6176430b219a8f3a53d30460045f
Yejin <yejin@Yejin-ui-MacBook-Air.local>
2015-10-24 18:35:31 -0400

    viewcontroller

--- YJKlicker/ViewController.swift
+++ YJKlicker/ViewController.swift
@@ -8,17 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController <UITableViewDataSource , UITableViewDelegate> {
 
+
+    @IBOutlet weak var drpdown: UIButton!
+    
+    @IBOutlet weak var TableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
