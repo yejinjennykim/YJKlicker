2d9074d4da8e9640a50ce6370aaffce8dc780368
Jee Wan Gue <treenson@gmail.com>
2015-10-25 08:27:12 +0900

    pickerview for school select added

--- YJKlicker/ViewController.swift
+++ YJKlicker/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController <UITableViewDataSource , UITableViewDelegate> {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
 
     @IBOutlet weak var drpdown: UIButton!
+    @IBOutlet weak var schoolPicker: UIPickerView!
+    var pickerDataSource = ["BU", "SNU"];
     
     @IBOutlet weak var TableView: UITableView!
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        self.schoolPicker.delegate = self;
+        self.schoolPicker.dataSource = self;
+        schoolPicker.hidden = true;
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
-
+    @IBAction func dropdownClicked(sender: AnyObject) {
+        if schoolPicker.hidden {
+            schoolPicker.hidden = false;
+        } else {
+            schoolPicker.hidden = true;
+        }
+    }
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count;
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerDataSource[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        drpdown.setTitle(pickerDataSource[row], forState: UIControlState.Normal)
+    }
 }
 
